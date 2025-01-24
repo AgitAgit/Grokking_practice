83bb804f5f7aa1df82fb2acb94a2d0d11f0e23ef
Amit Kubani <amitkubani@gmail.com>
2025-01-24 22:28:06 +0200

    commit

--- chapter_3_recursion/sandbox1.py
+++ chapter_3_recursion/sandbox1.py
@@ -6,3 +6,11 @@ def countdown(counter):
     countdown(counter - 1)
 
 # countdown(5)
+
+def factorial(num):
+    if(num == 0):
+        return 1
+    return num * factorial(num - 1)
+
+# result = factorial(4)
+# print(result)
