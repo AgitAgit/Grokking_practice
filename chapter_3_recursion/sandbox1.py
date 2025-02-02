53d0aa5f0b91eb2974c924c00e47620737c6fde8
Amit Kubani <amitkubani@gmail.com>
2025-02-02 08:02:21 +0200

    commit

--- chapter_3_recursion/sandbox1.py
+++ chapter_3_recursion/sandbox1.py
@@ -20,6 +20,11 @@ def sum(list):
         return 0
     return list[0] + sum(list[1:])
 
+def listLen(list):
+    if(list == []):
+        return 0
+    return 1 + listLen(list[1:])
+
 list1 = []
 list2 = [3]
 list3 = [5, 6, 7]
@@ -29,4 +34,19 @@ list4 = [-1, -3000, 0.2, 7, -1]
 # print(sum(list2))
 # print(sum(list3))
 # print(sum(list4))
+
+# print(listLen(list1))
+# print(listLen(list2))
+# print(listLen(list3))
+# print(listLen(list4))
+
+def maxNum(list, max):
+    if(list == []): return max
+    if(list[0] > max): return maxNum(list[1:], list[0])
+    return maxNum(list[1:], max)
+
+# print(maxNum(list1, 0))
+# print(maxNum(list2, 0))
+# print(maxNum(list3, 0))
+# print(maxNum(list4, 0))
 
